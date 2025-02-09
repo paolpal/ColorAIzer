16fc02c4017bef940a1f7f60421b79e06bbb0a48
Paolo Palumbo <paol.palumbo@gmail.com>
2025-02-09 19:49:58 +0100

    merged

--- colorizer.py
+++ colorizer.py
@@ -2,6 +2,7 @@ from torch import nn
 import torch
 import torchvision.transforms.functional as TF
 import torch.nn.functional as F
+import torch.nn.functional as F
 from tqdm import tqdm
 
 from imageset import ImageDataset
@@ -13,18 +14,27 @@ class Coloraizer(nn.Module):
 		# Encoder (Contrazione)
 		self.enc1 = self.conv_block(1, 32)    # output: [N, 32, H/2, W/2]
 		self.enc2 = self.conv_block(32, 64)  # output: [N, 64, H/4, W/4]
+		self.enc1 = self.conv_block(1, 32)    # output: [N, 32, H/2, W/2]
+		self.enc2 = self.conv_block(32, 64)  # output: [N, 64, H/4, W/4]
 		
 		# Bottleneck
 		# Sostituisci con un transformer, e 
 		# l'input testuale può esseere usato come condizionamento per ricolorare l'immagine
 		self.bottleneck = self.conv_block(64, 128) # output: [N, 128, H/8, W/8]
+		self.bottleneck = self.conv_block(64, 128) # output: [N, 128, H/8, W/8]
 		
 		# Decoder (Espansione)
 		self.dec2 = self.up_conv(128, 64)    # output: [N, 64, H/4, W/4]
 		self.dec1 = self.up_conv(64, 32)     # output: [N, 32, H/2, W/2]
+		self.dec2 = self.up_conv(128, 64)    # output: [N, 64, H/4, W/4]
+		self.dec1 = self.up_conv(64, 32)     # output: [N, 32, H/2, W/2]
 		
 		self.final_up = nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2)  # da [N, 32, H/2, W/2] a [N, 32, H, W]
+		self.final_up = nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2)  # da [N, 32, H/2, W/2] a [N, 32, H, W]
 		# Output: 2 canali (A e B dello spazio LAB)
+		self.final_layer = nn.Conv2d(32, 2, kernel_size=1) 
+
+		self.to(self.device) 
 		self.final_layer = nn.Conv2d(32, 2, kernel_size=1) 
 
 		self.to(self.device) 
@@ -47,6 +57,13 @@ class Coloraizer(nn.Module):
 		)
 	
 	def forward(self, x):
+		#print(f"{x.shape=}")
+		
+		# Encoder
+		enc1_out = self.enc1(x)
+		#print(f"{enc1_out.shape=}")
+		enc2_out = self.enc2(enc1_out)
+		#print(f"{enc2_out.shape=}")
 		#print(f"{x.shape=}")
 		
 		# Encoder
@@ -80,7 +97,33 @@ class Coloraizer(nn.Module):
 		output = self.final_layer(dec0_out)
 		#print(f"{output.shape=}")
 		
+		# Bottleneck
+		bottleneck_out = self.bottleneck(enc2_out)
+		#print(f"{bottleneck_out.shape=}")
+		
+		# Decoder - Step 2
+		dec2_out_raw = self.dec2(bottleneck_out)  # Raw output from the decoder
+		dec2_out_upsampled = F.interpolate(dec2_out_raw, size=enc2_out.shape[2:], mode='bilinear', align_corners=False)  # Upsampled output
+		dec2_out = dec2_out_upsampled + enc2_out  # Added to encoder output
+		#print(f"{dec2_out.shape=}")
+		
+		# Decoder - Step 3
+		dec1_out_raw = self.dec1(dec2_out)  # Raw output from the decoder
+		dec1_out_upsampled = F.interpolate(dec1_out_raw, size=enc1_out.shape[2:], mode='bilinear', align_corners=False)  # Upsampled output
+		dec1_out = dec1_out_upsampled + enc1_out  # Added to encoder output
+		#print(f"{dec1_out.shape=}")
+		
+		# Final upsampling
+		dec0_out_raw = self.final_up(dec1_out)  # Raw output from the final upconv layer
+		dec0_out = F.interpolate(dec0_out_raw, size=x.shape[2:], mode='bilinear', align_corners=False)  # Upsampled to original size
+		#print(f"{dec0_out.shape=}")
+		
+		# Final output
+		output = self.final_layer(dec0_out)
+		#print(f"{output.shape=}")
+		
 		return output
+
 
 	
 	def save(self, path: str):
@@ -103,20 +146,26 @@ class Coloraizer(nn.Module):
 		masked_mse = mse * mask  
 		loss = masked_mse.sum() / mask.sum()  
 		return loss  
+		loss = masked_mse.sum() / mask.sum()  
+		return loss  
 
 	
 	def fit(self, train: ImageDataset, valid: ImageDataset, epochs: int):
+		optim = torch.optim.Adam(self.parameters(), lr=1e-4)
 		optim = torch.optim.Adam(self.parameters(), lr=1e-4)
 		lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, patience=5, factor=0.5, threshold=0.01)
 		history = {"train": [], "valid": [], "lr": []}
 		
 		bs = 4
+		bs = 4
 		train_loader = torch.utils.data.DataLoader(
-			dataset=train, batch_size=bs, collate_fn=ImageDataset.collate_fn, num_workers=4,
+			dataset=train, batch_size=bs, collate_fn=ImageDataset.collate_fn,
 			sampler=torch.utils.data.RandomSampler(train, replacement=True, num_samples=bs * 100)
 		)
 		valid_loader = torch.utils.data.DataLoader(
 			dataset=valid, batch_size=bs, collate_fn=ImageDataset.collate_fn, num_workers=4,
+			sampler=torch.utils.data.RandomSampler(valid, replacement=True, num_samples=bs * 50)
+			dataset=valid, batch_size=bs, collate_fn=ImageDataset.collate_fn,
 			sampler=torch.utils.data.RandomSampler(valid, replacement=True, num_samples=bs * 50)
 		)
 
@@ -139,9 +188,11 @@ class Coloraizer(nn.Module):
 				loss = self.loss_fn(output, AB, mask)
 				loss.backward()
 				#torch.nn.utils.clip_grad_norm_(self.parameters(), 1)
+				#torch.nn.utils.clip_grad_norm_(self.parameters(), 1)
 				optim.step()
 				train_loss += loss.item()
 				batches.set_postfix(
+					loss=train_loss / (i + 1), lr=optim.param_groups[0]["lr"]
 					loss=train_loss / (i + 1), lr=optim.param_groups[0]["lr"]
 				)
 			
